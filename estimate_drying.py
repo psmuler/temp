42cda6e82534b6a0a966c3a6c647b341fef614cb
psmuler <94701070+psmuler@users.noreply.github.com>
2023-01-12 14:04:04 +0900

    Update estimate_drying.py

--- estimate_drying.py
+++ estimate_drying.py
@@ -16,7 +16,7 @@ table = pd.read_excel(path,header=[0],dtype=float).dropna()
 def f0(time, tau, b):
     return (1-b)*math.exp(-time/tau) + b
 def dif_f0_tau(time, tau, b):
-    return (1-b)*(math.exp(-time/tau))*(-time/(tau**2))
+    return (1-b)*(math.exp(-time/tau))*(time/(tau**2))
 def dif_f0_b(time, tau, b):
     return (-1)*math.exp(time/tau) + 1
 
@@ -41,19 +41,3 @@ while loss > tolerance:
     print(f"tau:{tau}, b:{b}, loss{loss}")
     if i > iteration:
         break
-    # loss = 0
-
-
-# x = [i*10 for i in range(100)]
-# y0 = [f0(xi,tau,b) for xi in x]
-# y1 = [dif_f0_tau(xi,tau,b) for xi in x]
-# y2 = [dif_f0_b(xi,tau,b) for xi in x]
-
-# fig, ax = plt.subplots(1, 1)
-# ax.set_title('all in one')
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# ax.plot(x,y0)
-# ax.plot(x,y1)
-# ax.plot(x,y2)
-# plt.show()
